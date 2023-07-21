2fa872807884c3bd420934ed6afae6bb0e0442f8
uttmangosteen <83084129+uttmangosteen@users.noreply.github.com>
2023-07-21 17:37:50 +0900

    Initial commit

---
+++ src/main/java/io/github/uttmangosteen/man10whitelist/Man10Whitelist.java
@@ -0,0 +1,27 @@
+package io.github.uttmangosteen.man10whitelist;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10Whitelist extends JavaPlugin {
+    JavaPlugin plugin;
+    @Override
+    public void onEnable() {
+        boolean onoff = Bukkit.hasWhitelist();
+        Bukkit.setWhitelist(true);
+        Bukkit.getLogger().info("[Man10whitelist]ホワイトリストをONにしました");
+        if (!onoff) {
+            int waitSeconds = plugin.getConfig().getInt("waitSeconds", 60);
+            if (waitSeconds < 0) return;
+            Bukkit.getLogger().info("[Man10whitelist]ホワイトリストを" + waitSeconds + "秒後にOFFにします");
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                Bukkit.setWhitelist(false);
+                Bukkit.getLogger().info("[Man10whitelist]ホワイトリストをOFFにしました");
+            }, waitSeconds * 20L);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
